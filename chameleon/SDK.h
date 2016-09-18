a90116a67d8c3e1991bf9565a2b6fe689a0962f6
aixxe <aixxe@skyenet.org>
2016-09-18 03:28:56 +0100

    Use RTLD_NOLOAD on dlopen.

--- chameleon/SDK.h
+++ chameleon/SDK.h
@@ -97,7 +97,7 @@ enum ItemDefinitionIndex: int {
 
 /* helper functions */
 template <typename interface> interface* GetInterface(const char* filename, const char* version) {
-	void* library = dlopen(filename, RTLD_NOW);
+	void* library = dlopen(filename, RTLD_NOLOAD | RTLD_NOW);
 
 	if (!library)
 		return nullptr;
